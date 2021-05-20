cb4702a3f3ddf865037038e9ed9ad60ae4af9c6a
roland <koffirolandndri@gmail.com>
2021-05-20 12:53:28 +0000

    j'ai modifier à nouveau

--- src/main/java/ExamenBigData.scala
+++ src/main/java/ExamenBigData.scala
@@ -8,8 +8,29 @@ object ExamenBigData {
         val new_list = liste.filter(s => s.endsWith("n"))
          return new_list
       }
-         // test de fla fonction
-      println(Listevaleur(List("Rolan","rrr","papap","pppp")))
+    println(Listevaleur(List("Julien","Paul","Jean","rac","trec","joel","ed","chris")))
+
+    // test de fla fonction
+
+        def liste_double():Unit={
+           val liste=List(Map(
+             "ecommermarg.fr"->"",
+             "https://www.journal.com/contact"->"Payant",
+             "https://www.zatap.com/"->"",
+             "https://lerevenu.com"->"",
+             "https://cadre-dirigeant-magazine.com"->"Paynt",
+             "https://silicon.fr/service/contact#/annoncer"->"Payant",
+             "https://www.channelbiz.fr/nous-contacter"->"",
+             "https://www.itespresso.fr"->"",
+             "https://www.industrie-mag.com/article/article4.html"->"invité",
+             "https://www.jesuissundev.com/article-invite"->"invite",
+             "https://www.journal.com/contact"->"invite"
+
+           )
+          )
+             // a ffichage ses resulatat des liste
+
+        }
 
   }
 }
